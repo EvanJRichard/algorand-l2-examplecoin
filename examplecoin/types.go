fe617ba33667c8098d413b4abb914d4bc277fc72
Evan Richard <evan.richard@algorand.com>
2019-03-01 10:36:26 -0500

    Add a Types and Utils file.

---
+++ examplecoin/types.go
@@ -0,0 +1,46 @@
+package examplecoin
+
+// Initialize represents the start of an examplecoin ledger.
+type Initialize struct {
+	_struct struct{} `codec:",omitempty,omitemptyarray"`
+
+	// Supply designates how much the examplecoin creator will start with.
+	Supply uint64 `codec:"supply"`
+}
+
+// Transfer represents an examplecoin transfer.
+type Transfer struct {
+	_struct struct{} `codec:",omitempty,omitemptyarray"`
+
+	// Destination designates who is receiving examplecoin.
+	Destination string `codec:"destination"`
+
+	// Amount designates how much examplecoin is being transferred.
+	Amount uint64 `codec:"Amount"`
+}
+
+// NoteFieldType indicates a type of examplecoin message encoded into a
+// transaction's Note field.
+type NoteFieldType string
+
+const (
+	// NoteInitialize indicates an Initialize message.
+	NoteInitialize NoteFieldType = "i"
+
+	// NoteTransfer indicates a Transfer message.
+	NoteTransfer NoteFieldType = "t"
+)
+
+// NoteField is the struct that represents an examplecoin message.
+type NoteField struct {
+	_struct struct{} `codec:",omitempty,omitemptyarray"`
+
+	// Type indicates which type of a message this is
+	Type NoteFieldType `codec:"type"`
+
+	// Initialize, for NoteInitialize type
+	Initialize Initialize `codec:"i"`
+
+	// Transfer, for NoteTransfer type
+	Transfer Transfer `codec:"t"`
+}
